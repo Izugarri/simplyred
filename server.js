0d650e1caa32a98ed2ffc5f92c36b0509d471d22
Izugarri <57817632+Izugarri@users.noreply.github.com>
2022-02-26 21:28:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname+'/'));
-
-console.log('escuchando en el puerto 8080');
-app.listen(80)
-app.listen(8080);
+app.use(express.static(__dirname+'/public'));
+console.log('escuchando en el puerto 15495 y 8080');
+app.listen(8080)
+app.listen(15495);
