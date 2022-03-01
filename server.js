1bd614442066ec93acc77f5795223a596bd5b3d8
Izugarri <57817632+Izugarri@users.noreply.github.com>
2022-03-01 18:53:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 3001
 
-app.use(express.static(__dirname+'/'));
-app.set('x-powered-by', false);
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+express()
+  .set('x-powered-by', false) // hide x-powered-by header!
+  .use(express.static(path.join(__dirname, '/')))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
