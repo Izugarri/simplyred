534646b298e288ffa6a027c7a3265029cf35a95a
Izugarri <57817632+Izugarri@users.noreply.github.com>
2022-03-01 15:36:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname+'/'));
-
+app.set('x-powered-by', false);
 console.log('escuchando en el puerto 3001');
 app.listen(3001);
