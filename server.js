2973eb1c65b4e6700e5c286399a0497ccd64c4d0
Izugarri <57817632+Izugarri@users.noreply.github.com>
2022-06-23 14:14:52 +0200

    no cookies youtube

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path')
-const PORT = process.env.PORT || 10000
-
-express()
-  .set('x-powered-by', false) // hide x-powered-by header!
-  .use(express.static(path.join(__dirname, '/')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
