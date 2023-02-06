af31689420824a3f6b4b9d1f38d8921cce2cf831
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-02-06 14:53:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,7 @@
-const express = require('express')
-const path = require("path");
-const app = express()
+const express = require('express');
+const app = express();
 
-// This configures static hosting for files in /public (para raiz './')that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('./', options));
-app.set('x-powered-by', false)
+app.use(express.static(__dirname+'/'));
 
-// Catch all handler for all other request.
-app.use('*', (req,res) => {
-  res.json({
-      at: new Date().toISOString(),
-      method: req.method,
-      hostname: req.hostname,
-      ip: req.ip,
-      query: req.query,
-      headers: req.headers,
-      cookies: req.cookies,
-      params: req.params
-    })
-    .end()
-})
-
-module.exports = app
-
-const port = process.env.PORT || 3000
-
-app.listen(port)
+console.log('escuchando en el puerto 3000');
+app.listen(3000);
