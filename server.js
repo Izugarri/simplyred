9e8fbc8091ddcc6e8d532052dd18f086ef946ae5
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-02-06 13:30:49 +0100

    PACKCAJE

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express')
+const path = require("path");
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
+// This configures static hosting for files in /public (para raiz './')that have the extensions
 // listed in the array.
 var options = {
   dotfiles: 'ignore',
@@ -16,11 +13,26 @@ var options = {
   redirect: false
 }
 app.use(express.static('./', options))
-// #############################################################################
 
+// Catch all handler for all other request.
+app.use('*', (req,res) => {
+  res.json({
+      at: new Date().toISOString(),
+      method: req.method,
+      hostname: req.hostname,
+      ip: req.ip,
+      query: req.query,
+      headers: req.headers,
+      cookies: req.cookies,
+      params: req.params
+    })
+    .end()
+})
 
-// Start the server
+module.exports = app
+
 const port = process.env.PORT || 3000
+
 app.listen(port, () => {
-  console.log(`server.js listening on ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
