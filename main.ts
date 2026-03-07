5f487dc61f62a21e19b623d084cc00e0e41fb384
izugarri3 <videomusicajmc@gmail.com>
2026-03-07 22:21:32 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,9 @@
-import { serveDir } from "jsr:@std/http/file-server";
-
-Deno.serve((req) => {
-  const pathname = new URL(req.url).pathname;
-
-  // Servir archivos estáticos desde una carpeta llamada "x"
-  if (pathname.startsWith("/")) {
-    return serveDir(req, {
-      fsRoot: "./", // Directorio donde están tus archivos .html y .css
-    }, {
+Deno.serve(() => {
+  return new Response("Static content that rarely changes", {
     headers: {
-      // Browser caches for 60s
-      "Cache-Control": "public, max-age=0",
-      // Deno Deploy CDN caches for 1 hour
-      "Deno-CDN-Cache-Control": "public, s-maxage=0",
+      "Cache-Control": "public, s-maxage=1",
+      // This response survives redeployments
+      "Deno-CDN-Cache-Control": "public, s-maxage=1",,
     },
   });
-  }
-
-  return new Response("404: Not Found", { status: 404 });
 });
