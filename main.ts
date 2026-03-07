91a97c48ec7a4261a03977e2054d0e60f6372cc6
izugarri3 <videomusicajmc@gmail.com>
2026-03-07 22:15:09 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,14 @@ Deno.serve((req) => {
   if (pathname.startsWith("/")) {
     return serveDir(req, {
       fsRoot: "./", // Directorio donde están tus archivos .html y .css
-    });
+    }, {
+    headers: {
+      // Browser caches for 60s
+      "Cache-Control": "public, max-age=0",
+      // Deno Deploy CDN caches for 1 hour
+      "Deno-CDN-Cache-Control": "public, s-maxage=0",
+    },
+  });
   }
 
   return new Response("404: Not Found", { status: 404 });
